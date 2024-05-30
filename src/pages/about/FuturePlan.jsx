9f8fb3c83118e6b107c928001dfc33d63ccd3c66
RohitN002 <rohitrandy002@gmail.com>
2024-05-30 19:39:14 +0530

    fUTURE PLAN

--- src/pages/about/FuturePlan.jsx
+++ src/pages/about/FuturePlan.jsx
@@ -3,24 +3,27 @@ import React from 'react'
 const FuturePlan = () => {
   return (
     <div><h3></h3>
-    <div> 
-      <p>To establish 50 educational support centers to prevent school dropout among the school going children of poor.</p>
-<p> To conduct school enrollment campaign at 150 places by setting up awareness cultural team</p>
-<p>To give self employment skill development and capacity building training to the women SHG members </p>
+    <div className='container'> 
+      <ul>
+
+      </ul>
+      <li>To establish 50 educational support centers to prevent school dropout among the school going children of poor.</li>
+<li> To conduct school enrollment campaign at 150 places by setting up awareness cultural team</li>
+<li>To give self employment skill development and capacity building training to the women SHG members </li>
 
 
-<p> To conduct health camps for the scavenging community and poor people in villages</p>
-<p>To give medical checkups to all those living in and prevent them from their drinking habits (alcohol consumption) and safe health condition </p>
+<li> To conduct health camps for the scavenging community and poor people in villages</li>
+<li>To give medical checkups to all those living in and prevent them from their drinking habits (alcohol consumption) and safe health condition </li>
 
-<p>Work with municipal bodies as well as housing colonies commercial complexes hospitals etc to provide safety equipments to sweepers and ensure their social security </p>
+<li>Work with municipal bodies as well as housing colonies commercial complexes hospitals etc to provide safety equipments to sweepers and ensure their social security </li>
 
-<p>Educate sweepers to engage in cleaning / sweeping work with use of safety equipments and technology
-</p>
-<p> Work in collaboration with corporate sectors, vocational training institute to provide vocational skill training to poor youths</p>
+<li>Educate sweepers to engage in cleaning / sweeping work with use of safety equipments and technology
+</li>
+<li> Work in collaboration with corporate sectors, vocational training institute to provide vocational skill training to poor youths</li>
 
 
 
-<p>To identify school drop out children and supporting for their education development in all villages</p>
+<li>To identify school drop out children and supporting for their education development in all villages</li>
 
 <p>To organize training for poor people about government welfare schemes</p>
 
@@ -30,8 +33,8 @@ const FuturePlan = () => {
 
 <p>To conduct training for poor parents in village about their children education and preventing child labour. </p>
 
-<p>To established women SHG’s and strengthening savings and prevented (Kanthu Vatti) private money lending</p>
-<p> Work towards social security (Insurance livelihood security, access to credit etc) of the poor people of villages</p>
+<li>To established women SHG’s and strengthening savings and prevented (Kanthu Vatti) private money lending</li>
+<li> Work towards social security (Insurance livelihood security, access to credit etc) of the poor people of villages</li>
 
 
 
